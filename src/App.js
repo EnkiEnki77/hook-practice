c6a737747323eb37a4ebb48453f019a6ef3a68c1
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2021-10-29 16:24:53 -0700

    homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useState, useEffect } from 'react'
+
+
 
 function App() {
+  
+
+  const [resourceType, setResourceType] = useState('todos')
+  const [items, setItems] = useState([])
+
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resourceType])
+
+  console.log(items)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+     <div>
+        <button onClick={()=>{setResourceType('todos')}}>Todo</button>
+        <button onClick={()=>{setResourceType('comments')}}>Comment</button>
+        <button onClick={()=>{setResourceType('posts')}}>Post</button>
+      </div>
+      <h1>{resourceType}</h1>
+      <div className='counter'>
+        <h2>data count:</h2>
+        <h2>{items.length}</h2>
+      </div>
+
+      {items.map(item => {
+        return <p>{JSON.stringify(item)}</p>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
